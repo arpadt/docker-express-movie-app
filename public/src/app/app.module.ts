6648c2089fd5c39004c40a02bdfd709bc5db085e
arpadt <a.arpad.toth@gmail.com>
2018-08-31 07:12:59 +1000

    Add search bar component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SidebarComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
