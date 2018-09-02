f83f193e8dfdf2aa00e6408dc8e18ae3b6ec6587
arpadt <a.arpad.toth@gmail.com>
2018-09-03 07:09:53 +1000

    Add ngFor to snippets. Start styling hover state on buttons.

--- public/src/app/movie-snippets/movie-snippets.component.ts
+++ public/src/app/movie-snippets/movie-snippets.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-snippets.component.scss']
 })
 export class MovieSnippetsComponent implements OnInit {
+  movieData = [
+    {
+      title: 'The Bourne Identity',
+      year: '2002',
+      rating: '14',
+      length: '119',
+      actors: 'Matt Damon, Franka Potente, Chris Cooper, Clive Owen',
+      // tslint:disable-next-line:max-line-length
+      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
+      imdbRating: '7.9',
+      poster: '../../assets/img/poster.jpg'
+    },
+    {
+      title: 'The Bourne Supremacy',
+      year: '2004',
+      rating: 'M',
+      length: '108',
+      actors: 'Matt Damon, Franka Potente, Joan Allen',
+      // tslint:disable-next-line:max-line-length
+      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
+      imdbRating: '7.8',
+      poster: '../../assets/img/poster.jpg'
+    },
+    {
+      title: 'The Bourne Ultimatum',
+      year: '2007',
+      rating: '16',
+      length: '115',
+      actors: 'Matt Damon, Edgar Ramirez, Joan Allen',
+      // tslint:disable-next-line:max-line-length
+      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
+      imdbRating: '8.0',
+      poster: '../../assets/img/poster.jpg'
+    }
+  ];
 
   constructor() { }
 
