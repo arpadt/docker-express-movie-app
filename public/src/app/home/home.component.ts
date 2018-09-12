d5ee645e05b7d0479caaaf6127345b597cbc7d17
arpadt <a.arpad.toth@gmail.com>
2018-09-12 21:29:15 +1000

    Refactor movie cards. Add request to the API.

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MovieDataService } from './../services/movie-data.service';
 import { environment } from './../../environments/environment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface Movies {
   Title: string;
   Year: string;
-  imdbId: string;
+  imdbID: string;
   Type: string;
   Poster: string;
   Rated?: string;
@@ -23,6 +24,10 @@ interface Movies {
   Website?: string;
 }
 
+interface TitleResults {
+  results: Movies[];
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -30,7 +35,34 @@ interface Movies {
 })
 export class HomeComponent implements OnInit {
   url = environment.apiUrl;
-  movies: Observable<Movies[]>;
+  // TODO: uncomment
+  // movies: Movies[];
+  movies = [
+    {
+      Title: 'The Bourne Ultimatum',
+      Year: '2007',
+      imdbID: 'tt0440963',
+      Type: 'movie',
+      // tslint:disable-next-line:max-line-length
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNGNiNmU2YTMtZmU4OS00MjM0LTlmYWUtMjVlYjAzYjE2N2RjXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg'
+    },
+    {
+      Title: 'The Bourne Identity',
+      Year: '2002',
+      imdbID: 'tt0258463',
+      Type: 'movie',
+      // tslint:disable-next-line:max-line-length
+      Poster: 'https://m.media-amazon.com/images/M/MV5BM2JkNGU0ZGMtZjVjNS00NjgyLWEyOWYtZmRmZGQyN2IxZjA2XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg'
+    },
+    {
+      Title: 'The Bourne Supremacy',
+      Year: '2004',
+      imdbID: 'tt0372183',
+      Type: 'movie',
+      // tslint:disable-next-line:max-line-length
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYTIyMDFmMmItMWQzYy00MjBiLTg2M2UtM2JiNDRhOWE4NjBhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
+    }
+  ];
 
   constructor(private movieDataService: MovieDataService) { }
 
@@ -40,6 +72,10 @@ export class HomeComponent implements OnInit {
   getMovies(title) {
     this.movieDataService
       .getMovieData(`${ this.url }/search/${ title }`)
-      .subscribe(res => console.log(res.body));
+      .subscribe((res) => {
+        const response = res.body as any;
+        this.movies = response.Search;
+        console.log(this.movies);
+      });
   }
 }
--- public/src/app/my-list/my-list.component.ts
+++ public/src/app/my-list/my-list.component.ts
@@ -8,52 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class MyListComponent implements OnInit {
   movieData = [
     {
-      title: 'The Bourne Identity',
-      year: '2002',
-      rated: '14',
-      length: '119',
-      language: 'English',
-      actors: 'Matt Damon, Franka Potente, Chris Cooper, Clive Owen',
-      // tslint:disable-next-line:max-line-length
-      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
-      imdbRating: '7.9',
-      poster: '../../assets/img/poster.jpg'
+      Title: 'The Bourne Identity',
+      Year: '2002',
+      Type: 'movie',
+      Poster: '../../assets/img/poster.jpg'
     },
     {
-      title: 'The Bourne Supremacy',
-      year: '2004',
-      rated: 'M',
-      length: '108',
-      language: 'English',
-      actors: 'Matt Damon, Franka Potente, Joan Allen',
-      // tslint:disable-next-line:max-line-length
-      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
-      imdbRating: '7.8',
-      poster: '../../assets/img/poster.jpg'
+      Title: 'The Bourne Supremacy',
+      Year: '2004',
+      Type: 'movie',
+      Poster: '../../assets/img/poster.jpg'
     },
     {
-      title: 'The Bourne Ultimatum',
-      year: '2007',
-      rated: '16',
-      length: '115',
-      language: 'English',
-      actors: 'Matt Damon, Edgar Ramirez, Joan Allen',
-      // tslint:disable-next-line:max-line-length
-      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
-      imdbRating: '8.0',
-      poster: '../../assets/img/poster.jpg'
+      Title: 'The Bourne Ultimatum',
+      Year: '2007',
+      Type: 'movie',
+      Poster: '../../assets/img/poster.jpg'
     },
     {
-      title: 'Jason Bourne',
-      year: '2016',
-      rated: '12A',
-      length: '123',
-      language: 'English',
-      actors: 'Matt Damon, Tommy Lee Jones, Alicia Vikander',
-      // tslint:disable-next-line:max-line-length
-      plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in lobortis tell et feugiat purus.Suspendisse convallis malesuada ultricies.',
-      imdbRating: '6.6',
-      poster: '../../assets/img/poster.jpg'
+      Title: 'Jason Bourne',
+      Year: '2016',
+      Type: 'movie',
+      Poster: '../../assets/img/poster.jpg'
     }
   ];
 
